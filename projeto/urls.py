0671ff0e97537e63dbbc5f4b344fbd4531303d4c
Juaoinacio <joaoizaias10@gmail.com>
2025-07-22 01:43:46 -0300

    continuando

--- projeto/urls.py
+++ projeto/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('', include('home.urls')),
     path('machine/', include('machine.urls')),
+    path('service/', include('service.urls')),
+
 ]
